849e5b540508042952a2c5f658104ebd11edb68d
Feroz Khan <muhammad.feroz@devsinc.com>
2021-12-08 18:34:38 +0500

    changing settings

--- todoApplicationDjango/settings.py
+++ todoApplicationDjango/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,7 @@ SECRET_KEY = 'django-insecure-r%h#91n8-u17yn3gdpwr$8+r&9a%1szpjq=%n#0ev4pu4cbs13
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'todo-application-python-django.herokuapp.com']
 
@@ -113,7 +115,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
